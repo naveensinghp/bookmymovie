2f4a3f7aa7e4784320f034a150a51eafe71507a5
NaveenSingh <naveensingh.mtp@gmail.com>
2023-06-08 00:34:29 +0530

    #Styled-Component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+import { COLORS } from '../../constants';
+
 
 function Header() {
   return (
-    <div className='header'>
-        <img src="assets/logo.png" alt="bookmymovielogo" width={200}/>
-    </div>
+    // <div className='header'>
+    //    
+    // </div>
+    <Wrapper>
+      <img src="assets/logo.png" alt="bookmymovielogo" width={200}/>
+    </Wrapper>
   );
 }
 
 export default Header;
+
+
+const Wrapper = styled.div`
+    position: sticky;
+    top: 0;
+    padding: 20px;
+    box-shadow: 0px 2px 5px hsl(0deg 0% 0% / 0.2); 
+    background: ${COLORS.gray[300]};
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    border-top: 1px solid #dddddd;
+    z-index: 1;
+`;
