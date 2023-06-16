7b6ff129476cbe5d826152520cbffa4d073053b2
NaveenSingh <naveensingh.mtp@gmail.com>
2023-06-16 23:52:04 +0530

    no message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import { useEffect, useRef, useState } from "react";
+
 import Header from "./components/Header/Header";
-import SparklesGlow from "./components/SparklesGlow/SparklesGlow";
 
 
 function App() {
   return (
    <Header>
    </Header>
-    // <>
-    //   <SparklesGlow>Rainbow Road</SparklesGlow>.
-    // </>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import SearchBar from '../SearchBar/SearchBar';
+import { Icon } from 'react-icons-kit'
+
+import { home } from 'react-icons-kit/icomoon/home'
 
 
 
 function Header() {
   return (
     <Wrapper>
-      <img src="assets/logo.png" alt="bookmymovielogo" width={200}/>
-      {/* <SearchBar /> */}
-      {/* <SearchBox></SearchBox> */}
+      <Logo>
+        <img src="assets/logo.png" alt="bookmymovielogo" width={200}/>
+      </Logo>
+      <Icon style={{marginLeft: "1rem", position: "absolute"}} size="1.5em" icon={home} />
       <Input 
           id="search-movie"
           type="text"
-          placeholder=' Search movies or theatres...'
+          placeholder='&#xF002;'
+          //font-family:Arial, FontAwesome
+          style={{fontFamily: 'Arial'}}
       />
       <ChooseLocation></ChooseLocation>
     </Wrapper>
@@ -23,9 +29,13 @@ function Header() {
 
 export default Header;
 
+const Logo = styled.div`
+  /* margin-top: 10px; */
+`;
+
 const Input = styled.input`
     height: 45px;
-    /* border-color: rgb(210, 210, 215); */
+    border-color: rgb(210, 210, 215);
     border-radius: 4px;
     border-style: solid;
     width: 40%;
@@ -38,7 +48,7 @@ const Wrapper = styled.div`
     width: 100%;
     top: 0;
     padding: 20px;
-    box-shadow: 0px 2px 5px hsl(0deg 0% 0% / 0.2); 
+    /* box-shadow: 0px 2px 5px hsl(0deg 0% 0% / 0.2);  */
     background: ${COLORS.gray[600]};
     display: flex;
     justify-content: space-between;
