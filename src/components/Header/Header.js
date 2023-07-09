7e8e28109536d1e50d69af13a4233cdb61161f1d
NaveenSingh <naveensingh.mtp@gmail.com>
2023-07-09 12:12:34 +0530

    Playground with HeaderPart

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,37 +22,57 @@ function Header() {
       <Logo>
         <img src="assets/logo.png" alt="bookmymovielogo" width={200}/>
       </Logo>
-      <SearchMovies>
-        <Icon style={{color: 'white'}} size="0.9em" icon={search} />
-        <Input 
-            id=" search-movie"
-            type="text"
-            placeholder="Search movies or theatres"
-        />
-         <Icon style={{color: 'white',cursor: 'pointer'}} size="1.4em" icon={ic_mic_outline} /> 
-      </SearchMovies>
-      <ChooseLocation>
-          Coimbatore
-      </ChooseLocation>
+      <div style={{background: 'hotpink',display:'flex',justifyContent: 'space-around',alignItems: 'center',paddingLeft: '7%'}}>
+          <SearchMovies>
+            <Icon style={{color: 'white'}} size="0.9em" icon={search} />
+            <Input 
+                id=" search-movie"
+                type="text"
+                placeholder="Search movies or theatres"
+            />
+            <Icon style={{color: 'white',cursor: 'pointer'}} size="1.4em" icon={ic_mic_outline} /> 
+          </SearchMovies>
+          <Select>
+            <option value="">- Select Bank -</option>
+            <option value="coim">Coimbatore</option>
+          </Select>
+      </div>
       <MenuIcon>
-        { isOpened ? <Icon size={42} icon={iosMoon} onClick={toggle} /> 
-          : <Icon size={32} icon={adjustBrightness} onClick={toggle} />
-        }
-      </MenuIcon>
+          { isOpened ? <Icon size={42} icon={iosMoon} onClick={toggle} /> 
+            : <Icon size={32} icon={adjustBrightness} onClick={toggle} />
+          }
+        </MenuIcon>
+      
+      {/* <ChooseLocation>
+          Coimbatore
+      </ChooseLocation> */}
+      
+      
     </Wrapper>
   );
 }
 
 export default Header;
 
+const Select = styled.select`
+    background: ${COLORS.gray[300]};
+    /* width: 20%; */
+    padding: 0 15px;
+    color: white;
+    border-color: ${COLORS.gray[300]};
+    font-size: 14px;
+    border-radius: 8px;
+    color: ${COLORS.white};
+    height: 2.5rem;
+`;
 const SearchMovies = styled.div`
   background: ${COLORS.gray[300]};
-  width: 40%;
-  border-radius: 10px;
+  width: 50%;
+  border-radius: 5px;
   height: 2.5rem;
   padding: 0 15px;
   display: flex;
-  align-items: center;
+  align-items: baseline;
 `;
 
 const Test = styled.div`
@@ -95,8 +115,9 @@ const Wrapper = styled.div`
     box-shadow: 0px 2px 5px hsl(0deg 0% 0% / 0.2); 
     background: ${COLORS.gray[600]};
     display: flex;
+    /* display: flex;
     justify-content: space-between;
-    align-items: flex-start;
+    align-items: flex-start; */
 `;
 
 const SearchBox = styled.div`
@@ -116,7 +137,7 @@ const ChooseLocation = styled.div`
   align-items: end;
   color: ${COLORS.white};
   background: ${COLORS.gray[300]};
-  width: 350px;
+  width: 300px;
   height: 40px;
   border-radius: 12px;
 `;
