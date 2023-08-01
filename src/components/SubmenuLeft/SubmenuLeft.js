41c19de28a3a41e8029a995066d151af0c8956b5
NaveenSingh <naveensingh.mtp@gmail.com>
2023-08-02 00:23:42 +0530

    no message

--- src/components/SubmenuLeft/SubmenuLeft.js
+++ src/components/SubmenuLeft/SubmenuLeft.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import { home } from 'react-icons-kit/icomoon/home'
 import { Icon } from 'react-icons-kit'
+import styled from 'styled-components';
+
 
 function SubmenuLeft() {
-  return <div className="threemenu" >
-        <ul style={{listStyleType: 'none',display: 'flex',gap: '20px',fontWeight: 'bold',fontSize: '12px'}}>
-          <li> <Icon style={{color: 'white'}} icon={home} /></li>
-          <li>MAX</li>
-          <li>4DX</li>
-          <li>Playhouse</li>
-          <li>GOLD</li>
-          <li>P[XL]</li>
-          <li>ONYX</li>
-        </ul>
+  return(
+    <>
+      <div className="threemenu">
+        <Wrapper>
+          <Icon style={{color: 'white'}} icon={home} />
+          <Ul>
+            <li>MAX</li>
+            <li>4DX</li>
+            <li>Playhouse</li>
+            <li>GOLD</li>
+            <li>P[XL]</li>
+            <li>ONYX</li>
+          </Ul>
+        </Wrapper>
       </div>;
+    </>
+  )
 }
 
 export default SubmenuLeft;
+
+
+const Wrapper = styled.div`
+  background: hotpink;
+`;
+
+
+const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  font-weight: bold;
+  font-size: 12px;
+`;
