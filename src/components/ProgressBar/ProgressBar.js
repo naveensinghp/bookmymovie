95794a1ef15dbd6004d655773cbe7d2c977a0cce
NaveenSingh <naveensingh.mtp@gmail.com>
2023-08-24 08:26:32 +0530

    no message

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const STYLES = {
+  small: {
+    height: 8,
+    padding: 0,
+  },
+  medium:{
+    height: 12,
+    padding: 0,
+  },
+  large:{
+    height: 16,
+    padding: 4,
+  }
+} 
+
 const ProgressBar = ({value,size}) =>{
+
+  const styles = STYLES[size];
+  if(!styles){
+    throw new Error(`Unknown Size Passed to ProgressBar: ${size}`);
+  }
   return <Wrapper>
     <div
       role="progressbar"
       aria-valuenow={value}
       aria-valuemin="0"
       aria-valuemax="100"
+      style={{
+        '--padding': styles.padding + 'px',
+      }}
     >
-      <Bar style={{'--width': value + '%'}} />
+      <Bar 
+        style={{
+          '--width': value + '%',
+          '--height': styles.height + 'px'
+        }} />
     </div>
 </Wrapper>
 }
@@ -20,10 +47,12 @@ const Wrapper = styled.div`
   box-shadow: inset 0px 2px 4px #9E9E9E;
   border-radius: 4px;
   overflow: hidden;
+  padding: var(--padding);
 `
 
 const Bar = styled.div`
   width: var(--width);
+  height: var(--height);
   height: 8px;
   background-color: hotpink;
   border-radius: 4px 0 0 4px;
