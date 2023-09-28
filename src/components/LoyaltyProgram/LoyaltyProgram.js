b4b4fe727c354aa1850c806792515e7372610c6a
NaveenSingh <naveensingh.mtp@gmail.com>
2023-09-28 23:52:22 +0530

    Loyality Program

--- src/components/LoyaltyProgram/LoyaltyProgram.js
+++ src/components/LoyaltyProgram/LoyaltyProgram.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import Button from '../Button/Button';
+
 
 
 function LoyaltyProgram() {
   return (
     <Card>
-
+      <div style={{display: 'flex',flexDirection: 'column'}}> 
+        <TitleText>
+            Introducing a loyalty program with special rewards and exclusive benefits!
+        </TitleText>
+        <Button>Know More</Button>
+      </div>
     </Card>
   );
 }
@@ -15,7 +22,14 @@ export default LoyaltyProgram;
 const Card = styled.div`
   display: flex;
   justify-content: center;
-  width: 300px;
-  height: 400px;
-  background-color: var(--test-color, yellow);
+  width: 800px;
+  height: 240px;
+  margin-left: 15%;
+  border-radius: 12px;
+  background-color: var(--color-black-400, yellow);
+`;
+
+const TitleText = styled.div`
+  font-size: 1.3rem;
+  color: var(--color-gray-900);
 `;
