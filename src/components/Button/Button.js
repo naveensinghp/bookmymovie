884befdf26a78b4184b6a119ab2941a6b1917619
NaveenSingh <naveensingh.mtp@gmail.com>
2023-10-27 10:06:13 +0530

    no message

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,12 +5,12 @@ const SIZES = {
   small: {
     '--borderRadius': 2 + 'px',
     '--fontSize': 16 / 16 + "rem",
-    '--padding': "8px 24px",
+    '--padding': "8px 12px",
   },
   medium: {
     '--borderRadius': 2 + 'px',
     '--fontSize': 18 / 16 + "rem",
-    '--padding': "16px 24px",
+    '--padding': "12px 20px",
   },
   large: {
     '--borderRadius': 2 + 'px',
@@ -30,7 +30,7 @@ function Button({variant,size,children}) {
   }
   return(
     <>
-    <Component style={styles}>
+    <Component style={styles} as="a">
       {children}
     </Component>
     </>
@@ -39,7 +39,7 @@ function Button({variant,size,children}) {
   
 }
 
-const ButtonElem = styled.button`
+const ButtonBase = styled.button`
   min-height: 32px;
   background-color: var(--color-white);
   color: var(--color-black);
@@ -53,19 +53,25 @@ const ButtonElem = styled.button`
   /* font-size: 0.9rem; */
   font-size: var(--fontSize);
   letter-spacing: 0.6px;
+
+
   cursor: pointer;
+  &:focus {
+    outline-color: hotpink;
+    outline-offset: 4px;
+  }
 `;
 
-const FillButton = styled(ButtonElem)`
+const FillButton = styled(ButtonBase)`
   background-color: 'hotpink';
   color: 'white';
 `
-const OutlineButton = styled(ButtonElem)`
+const OutlineButton = styled(ButtonBase)`
   background-color: 'red';
   color: 'white';
   border: 2px solid
 `
-const GhostButton = styled(ButtonElem)``
+const GhostButton = styled(ButtonBase)``
 
 
 export default Button;
