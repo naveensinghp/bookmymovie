632f96fe98ac904d25647eae82b6b2591ec5d72d
NaveenSingh <naveensingh.mtp@gmail.com>
2023-11-19 00:55:51 +0530

    closebutton

--- src/pages/moviecalendar.js
+++ src/pages/moviecalendar.js
@@ -1,7 +1,8 @@
 import Header from "../components/Header/Header";
 import TextLink from "../components/TextLink";
 import styled from 'styled-components';
-
+import { X } from 'react-feather';
+import Link from "../components/Link";
 function MovieCalendar(){
     return(
         <>
@@ -9,8 +10,14 @@ function MovieCalendar(){
             <TextLink  href="/">Go Home</TextLink>
             {/* <h1>Movie Calendar Page</h1> */}
             <Wrapper>
+                <Close>
+                    <Link href="/">
+                        <X size={32} color="white" />
+                    </Link>
+                </Close>
                 {/* MOVIE BANNER GOESHERS */}
                 <img src="https://originserver-static1-uat.pvrcinemas.com/newweb/movies/big/1460x600/HO00025768.jpg" alt="banner" />
+                
                 <MoviePoster>
                     <Showwrapper>
                         <Showstuff> ShowTimes</Showstuff>
@@ -28,7 +35,12 @@ export default MovieCalendar;
 
 const MoviePoster = styled.div`
 `
-
+const Close = styled.div`
+    cursor: pointer;
+    position: absolute;
+    right: 20px;
+    top: 130px;
+`;
 
 const Showwrapper = styled.div`
     display: flex;
