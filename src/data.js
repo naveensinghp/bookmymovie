15d7964619efa763b7cdec90d6eb9eb3aa41b6a7
NaveenSingh <naveensingh.mtp@gmail.com>
2024-02-20 00:18:14 +0530

    no message

--- src/data.js
+++ src/data.js
@@ -106,7 +106,7 @@ export const dummyShowTime = [
 export const seatNumber = [
     {
         id:1,
-        seat: "a",
+        seat: "a ",
         seatno: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
     },
     {
