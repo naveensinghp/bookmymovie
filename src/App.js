9ad1b41ea3e2cd9cce4d36532da1f68417d0ffa0
NaveenSingh <naveensingh.mtp@gmail.com>
2024-02-26 23:49:32 +0530

    no message

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           <Route exact path="/fm" element={<FramerMotionLearnings />} />
         </Routes>
       </Router>
-      {/* <FramerMotionLearnings /> */}
+     
     </>
   );
 }
