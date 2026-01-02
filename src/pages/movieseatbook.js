08560a7e11dc33c2235e962af14cd180c9d77583
NaveenSingh <naveensingh.mtp@gmail.com>
2026-01-02 23:54:17 +0530

    no message

--- src/pages/movieseatbook.js
+++ src/pages/movieseatbook.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 function BookMovieSeat(){
     return(
         <>
-            <Header />
+            {/* <Header /> */}
             <Wrapper>
                 <MovieSeatBook />
             </Wrapper>
