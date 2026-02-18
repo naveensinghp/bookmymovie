04ba6d2e6025646f2ed67a800f99c11d31f5b8ad
NaveenSingh <naveensingh.mtp@gmail.com>
2026-02-18 23:58:09 +0530

    no message

--- src/components/MovieDetailedInfo/MovieDetailedInfo.js
+++ src/components/MovieDetailedInfo/MovieDetailedInfo.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
 
 function MovieDetailedInfo() {
-  return <div></div>;
+  return <>
+    <Wrapper>
+
+    </Wrapper>
+  </>;
 }
 
 export default MovieDetailedInfo;
+
+const Wrapper = styled.div`
+
+
+`;
