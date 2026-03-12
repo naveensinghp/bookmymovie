b1dca3d2141c259ac4d3ca997f2c2b79d5406ce4
NaveenSingh <naveensingh.mtp@gmail.com>
2026-03-12 23:02:47 +0530

    no message

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -2,75 +2,76 @@ import React from 'react';
 import styles from './MovieCard.module.css';
 import Button from '../Button/Button';
 import { Heart } from 'react-feather';
-import Spinner from '../Spinner';
-import TextLink from '../TextLink/TextLink';
-import MovieCalendar from '../../pages/moviecalendar';
-import { Link } from "react-router-dom"; 
-import { useNavigate } from 'react-router-dom';
-
-
+import { Link, useNavigate } from "react-router-dom";
 
 function MovieCard(props) {
+
   const navigate = useNavigate();
-   let movie = props.data;
-   // console.log(movie);
-   const [showMovieDetails, setShowMovieDetails] = React.useState(false);
-   const[like, setLink] = React.useState(movie.likesCount);
-   function onClickLike(){
-    setLink(like +1)
-   }
+  const movie = props.data;
+
+  const [like, setLike] = React.useState(movie.likesCount);
+
+  function onClickLike() {
+    setLike(like + 1);
+  }
+
   const goToMovieCalendar = () => {
     navigate(`/movie/${movie.id}`);
-  }
-  // const handleClick = () => {
-  //   navigate('/moviecalendar', {
-  //     state: {
-  //       title: "NaveenSingh"
-  //     },
-  //   });
-  // };
-  return(
-    <>
-     {/* <Spinner /> */}
-      <div className={styles.cardwrapper}>
+  };
+
+  return (
+    <div className={styles.cardwrapper}>
+      
       <div className={styles.wrapper}>
-      <a href="/" className={styles.thumbnailwrapper}>
-           <img src={movie.moviePoster} 
-          className={styles.movieimg} 
-          alt="bookmymovielogo"/>
-        </a>
+        <Link to={`/movie/${movie.id}`} className={styles.thumbnailwrapper}>
+          <img
+            src={movie.moviePoster}
+            className={styles.movieimg}
+            alt={movie.movieName}
+          />
+        </Link>
       </div>
-        <div>
-          { movie.newRelease ? <div className={styles.newreleaselabel}> Releasing on {movie.releaseDate}</div> : ""}
-          <div className={styles.likeheart}>
-            <Heart size={18} color='white' onClick={onClickLike} />
+
+      <div>
+        {movie.newRelease && (
+          <div className={styles.newreleaselabel}>
+            Releasing on {movie.releaseDate}
           </div>
-          <div className={styles.likescount}>
-              {like}k
+        )}
+
+        <div className={styles.likeheart}>
+          <Heart size={18} color="white" onClick={onClickLike} />
+        </div>
+
+        <div className={styles.likescount}>
+          {like}k
+        </div>
+      </div>
+
+      <div className={styles.moviehover}>
+        <div className={styles.moviename}>
+          <h4 className={styles.movietitle}>{movie.movieName}</h4>
+          <div>
+            {movie.language} • (U/A) • {movie.genre}
           </div>
         </div>
-         {/* Entire movie set hover */}
-        <div className={styles.moviehover}>
-          <div className={styles.moviename}>
-              <h4 className={styles.movietitle}>{movie.movieName}</h4>
-              <div>
-                {movie.language} • (U/A) • {movie.genre}
-              </div>
-          </div>
-          <div className={styles.bookingstuff}>
-            <Button 
-                variant='fill' 
-                size="small" 
-                onClick={() => alert('Booking Tickets for ' + movie.movieName)}>
-                  Book Tickets
-              </Button>
-              {/* <button onClick={() => showMovieDetails(true)}>
-                Book Ticket</button> */}
-            <Button variant='fill' size="small">Play Trailer</Button>
-          </div>
-        </div> 
+
+        <div className={styles.bookingstuff}>
+          <Button
+            variant="fill"
+            size="small"
+            onClick={goToMovieCalendar}
+          >
+            Book Tickets
+          </Button>
+
+          <Button variant="fill" size="small">
+            Play Trailer
+          </Button>
+        </div>
       </div>
-    </>
+
+    </div>
   );
 }
 
