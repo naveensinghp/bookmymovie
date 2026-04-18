0e48159b39a2c3154cfc48f7b74035eb461898b9
NaveenSingh <naveensingh.mtp@gmail.com>
2026-04-18 23:50:19 +0530

    no message

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,11 +1,25 @@
 import styled from 'styled-components';
+import React from 'react';
+
 
 
  function Test(){
+  const [count,setCount] = React.useState(0);
   return(
     <>
-
       <h1>Hello World</h1>
+      <p>
+        You've clicked {count} times.
+      </p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+
+          console.log(count)
+        }}
+      >
+        Click me!
+      </button>
     </>
   );
 }
